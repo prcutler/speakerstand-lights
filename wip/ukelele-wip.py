600dba19207a558c16c03b00e3fa70b3bf08468b
Paul Cutler <paul@paulcutler.org>
2021-08-09 18:51:00 -0500

    Update vu-meter-wip with working analog mic

--- wip/ukelele-wip.py
+++ wip/ukelele-wip.py
@@ -16,13 +16,11 @@ Pluck high A♭ on the E string to cycle through the animation modes
 
 import time
 import array
-import digitalio
-import audiobusio
+from adafruit_led_animation.animation import rainbow
 import board
 import neopixel
 from ulab.scipy.signal import spectrogram
 from ulab import numpy as np
-import adafruit_lsm6ds
 from adafruit_led_animation.helper import PixelMap
 from adafruit_led_animation.sequence import AnimationSequence
 from adafruit_led_animation.group import AnimationGroup
@@ -41,6 +39,8 @@ from adafruit_led_animation.color import (
     PURPLE,
     WHITE,
 )
+# Add analogio for analog mic
+from analogio import AnalogIn
 
 MAX_BRIGHTNESS = 0.3 #set max brightness for sound reactive mode
 NORMAL_BRIGHTNESS = 0.1 #set brightness for non-reactive mode
@@ -55,41 +55,90 @@ pixels = neopixel.NeoPixel(NEOPIXEL_PIN, NUM_PIXELS, brightness=1, auto_write=Fa
 pixels.fill(0)  # NeoPixels off ASAP on startup
 pixels.show()
 
-mic = board.A2  # set mic input
+pixelmap_fw = PixelMap(pixels, [
+    0, 1, 2, 3, 4, 5, 6, 7,
+    8, 9, 10, 11, 12, 13, 14, 15, 
+    16, 17, 18, 19, 20, 21, 22, 23,
+    24, 25, 26, 27, 28, 29, 30, 31], individual_pixels=True)
 
+pixel_map = pixelmap_fw
+
+mic = AnalogIn(board.A2)  # set mic input
+NUM_SAMPLES = 256
+samples_bit = array.array('H', [0] * (NUM_SAMPLES+3))
+
+# Cusomize LED Animations  ------------------------------------------------------
+powerup = RainbowComet(pixel_map, speed=0, tail_length=25, bounce=False)
+rainbow = Rainbow(pixel_map, speed=0, period=6, name="rainbow", step=2.4)
+rainbow_chase = RainbowChase(pixel_map, speed=0, size=3, spacing=15, step=10)
+rainbow_chase2 = RainbowChase(pixel_map, speed=0, size=10, spacing=1, step=18)
+chase = Chase(pixel_map, speed=0.1, color=RED, size=1, spacing=6)
+rainbow_comet = RainbowComet(pixel_map, speed=0, tail_length=80, bounce=True)
+rainbow_comet2 = RainbowComet(
+    pixel_map, speed=0, tail_length=104, colorwheel_offset=80, bounce=True
+    )
+rainbow_comet3 = RainbowComet(
+    pixel_map, speed=0, tail_length=25, colorwheel_offset=80, step=4, bounce=False
+    )
+strum = RainbowComet(
+    pixel_map, speed=0, tail_length=25, bounce=False, colorwheel_offset=50, step=4
+    )
+lava = Comet(pixel_map, speed=0.01, color=ORANGE, tail_length=40, bounce=False)
+sparkle = Sparkle(pixel_map, speed=0.01, color=BLUE, num_sparkles=10)
+sparkle2 = Sparkle(pixel_map, speed=0.05, color=PURPLE, num_sparkles=4)
+
+# Animations Playlist - reorder as desired. AnimationGroups play at the same time
+animations = AnimationSequence(
+    rainbow,
+    rainbow_chase,
+    rainbow_chase2,
+    chase,
+    lava,
+    rainbow_comet,
+    rainbow_comet2,
+    AnimationGroup(
+        sparkle,
+        strum,
+        ),
+    AnimationGroup(
+        sparkle2,
+        rainbow_comet3,
+        ),
+    auto_clear=True,
+    auto_reset=True,
+)
 
 # Main loop
 while True:
-    i = (i + 0.5) % 256  # run from 0 to 255
-    TILT_COLOR = colorwheel(i)
-    if MODE == 0:  # If currently off...
-        enable.value = True
-        power_on(POWER_ON_DURATION)  # Power up!
-        MODE = LASTMODE
+    # Insert Analog mic code below commented out code
+    # mic.record(samples_bit, len(samples_bit))
+    samples = mic.value
+    # (The mic is live, but no display)
 
-    elif MODE >= 1:  # If not OFF MODE...
-        mic.record(samples_bit, len(samples_bit))
-        samples = np.array(samples_bit[3:])
-        spectrum = spectrogram(samples)
-        spectrum = spectrum[:128]
-        spectrum[0] = 0
-        spectrum[1] = 0
-        peak_idx = np.argmax(spectrum)
-        peak_freq = peak_idx * 16000 / 256
+    samples = np.array(samples_bit[3:])
+    spectrum = spectrogram(samples)
+    spectrum = spectrum[:128]
+    spectrum[0] = 0
+    spectrum[1] = 0
+    peak_idx = np.argmax(spectrum)
+    peak_freq = peak_idx * 16000 / 256
 #        print((peak_idx, peak_freq, spectrum[peak_idx]))
-        magnitude = spectrum[peak_idx]
+    magnitude = spectrum[peak_idx]
 #         time.sleep(1)
-        if peak_freq == 812.50 and magnitude > SOUND_THRESHOLD:
-            animations.next()
+    if peak_freq == 812.50 and magnitude > SOUND_THRESHOLD:
+        animations.next()
+        time.sleep(1)
+    if peak_freq == 875 and magnitude > SOUND_THRESHOLD:
+        if MODE == 1:
+            MODE = 2
+            print("mode = 2")
+            LASTMODE = 2
             time.sleep(1)
-        if peak_freq == 875 and magnitude > SOUND_THRESHOLD:
-            if MODE == 1:
-                MODE = 2
-                print("mode = 2")
-                LASTMODE = 2
-                time.sleep(1)
-            elif MODE == 2:
-                MODE = 1
-                print("mode = 1")
-                LASTMODE = 1
-                time.sleep(1)
+        elif MODE == 2:
+            MODE = 1
+            print("mode = 1")
+            LASTMODE = 1
+            time.sleep(1)
+
+
+
