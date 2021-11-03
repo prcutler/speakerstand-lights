5bafbbc89548b50091da7a6c70c7d21fefe64735
Paul Cutler <paul@paulcutler.org>
2021-11-03 13:22:42 -0500

    Now I know how Dr Frankenstein felt -  It worked

--- code.py
+++ code.py
@@ -2,7 +2,7 @@
 From https://github.com/adafruit/Adafruit_Learning_System_Guides/blob/main/EyeLights_Audio_Spectrum/code.py 
 """
 
-import array import array
+from array import array
 from math import log
 import board
 import neopixel
@@ -11,6 +11,7 @@ from ulab import numpy as np
 from ulab.scipy.signal import spectrogram
 from supervisor import reload
 from rainbowio import colorwheel
+import time 
 
 
 # Digital Audio 
@@ -37,7 +38,7 @@ high_bin = 75  # Highest bin "
 
 # Set NeoPixel
 pixel_pin = board.D6  # NeoPixel LED strand is connected to GPIO #0 / D0
-n_pixels = 32  # Number of pixels you are using
+pixels = 32  # Number of pixels you are using
 
 
 # Add Neopixel Featherwing vertical and horizontal functions
@@ -55,7 +56,6 @@ pixel_framebuf = PixelFramebuffer(
     pixels,
     pixel_width,
     pixel_height,
-    brightness=0.1,
     alternating=False,
 )
 
@@ -63,6 +63,7 @@ pixel_framebuf = PixelFramebuffer(
 # Set up digial mic
 mic = audiobusio.PDMIn(board.D10, board.D9,
                        sample_rate=16000, bit_depth=16)
+rec_buf = array("H", [0] * fft_size)  # 16-bit audio samples
 
 # FFT/SPECTRUM SETUP -----
 
@@ -107,3 +108,93 @@ for column in range(pixel_width):
     bin_weights = [
         (weight / total) * (0.8 + idx / pixel_width * 1.4)
         for idx, weight in enumerate(bin_weights)
+        ]
+    # List w/five elements is stored for each column:
+    # 0: Index of the first spectrum bin that impacts this column.
+    # 1: A list of bin weights, starting from index above, length varies.
+    # 2: Color for drawing this column on the LED matrix. The 225 is on
+    #    purpose, providing hues from red to purple, leaving out magenta.
+    # 3: Current height of the 'falling dot', updated each frame
+    # 4: Current velocity of the 'falling dot', updated each frame
+    column_table.append(
+        [
+            first_bin - low_bin,
+            bin_weights,
+            colorwheel(225 * column / pixel_width),
+            pixel_height,
+            0.0,
+        ]
+    )
+print(column_table)
+
+
+# MAIN LOOP -------------
+
+dynamic_level = 10  # For responding to changing volume levels
+frames, start_time = 0, time.monotonic()  # For frames-per-second calc
+
+while True:
+    # The try/except here is because VERY INFREQUENTLY the I2C bus will
+    # encounter an error when accessing the LED driver, whether from bumping
+    # around the wires or sometimes an I2C device just gets wedged. To more
+    # robustly handle the latter, the code will restart if that happens.
+    try:
+        mic.record(rec_buf, fft_size)  # Record batch of 16-bit samples
+        samples = np.array(rec_buf)  # Convert to ndarray
+        # Compute spectrogram and trim results. Only the left half is
+        # normally needed (right half is mirrored), but we trim further as
+        # only the low_bin to high_bin elements are interesting to graph.
+        spectrum = spectrogram(samples)[low_bin : high_bin + 1]
+        # Linearize spectrum output. spectrogram() is always nonnegative,
+        # but add a tiny value to change any zeros to nonzero numbers
+        # (avoids rare 'inf' error)
+        spectrum = np.log(spectrum + 1e-7)
+        # Determine minimum & maximum across all spectrum bins, with limits
+        lower = max(np.min(spectrum), 4)
+        upper = min(max(np.max(spectrum), lower + 6), 20)
+
+        # Adjust dynamic level to current spectrum output, keeps the graph
+        # 'lively' as ambient volume changes. Sparkle but don't saturate.
+        if upper > dynamic_level:
+            # Got louder. Move level up quickly but allow initial "bump."
+            dynamic_level = upper * 0.7 + dynamic_level * 0.3
+        else:
+            # Got quieter. Ease level down, else too many bumps.
+            dynamic_level = dynamic_level * 0.5 + lower * 0.5
+
+        # Apply vertical scale to spectrum data. Results may exceed
+        # matrix height...that's OK, adds impact!
+        data = (spectrum - lower) * (7 / (dynamic_level - lower))
+
+        for column, element in enumerate(column_table):
+            # Start BELOW matrix and accumulate bin weights UP, saves math
+            first_bin = element[0]
+            column_top = pixel_height + 1
+            for bin_offset, weight in enumerate(element[1]):
+                column_top -= data[first_bin + bin_offset] * weight
+
+            if column_top < element[3]:  #       Above current falling dot?
+                element[3] = column_top - 0.5  # Move dot up
+                element[4] = 0  #                and clear out velocity
+            else:
+                element[3] += element[4]  #      Move dot down
+                element[4] += 0.2  #             and accelerate
+
+            column_top = int(column_top)  #      Quantize to pixel space
+            for row in range(column_top):  #     Erase area above column
+                pixel_framebuf.pixel(column, row, 0)
+                # glasses.pixel(column, row, 0)
+            for row in range(column_top, 5):  #  Draw column
+                pixel_framebuf.pixel(column, row, element[2])
+
+            pixel_framebuf.pixel(column, int(element[3]), 0xE08080)  # Draw peak dot
+
+        # glasses.show()  # Buffered mode MUST use show() to refresh matrix
+        pixel_framebuf.display()
+
+        frames += 1
+        # print(frames / (monotonic() - start_time), "FPS")
+
+    except OSError:  # See "try" notes above regarding rare I2C errors.
+        print("Restarting")
+        reload()
